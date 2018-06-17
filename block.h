16ea76b91377a4ab167cbb00ada13a9b0855d441
alexxia <tx2126@columbia.edu>
2018-06-16 22:24:41 -0400

    Initial commit.

--- block.h
+++ block.h
@@ -5,6 +5,27 @@
 #ifndef IDCHAIN_BLOCK_H
 #define IDCHAIN_BLOCK_H
 
-// WTF it is?
+#include <cstdint>
+#include <iostream>
+
+class Block {
+public:
+    string sPrevHash;
+
+    Block(uint32_t nIndexIn, const string &sDataIn);
+
+    string GetHash();
+
+    void MineBlock(uint32_t nDifficulty);
+
+private:
+    uint32_t nIndex_;
+    int64_t nNonce_;
+    string sData_;
+    string sHash_;
+    time_t tTime_;
+
+    string CalculateHash() const;
+};
 
 #endif //IDCHAIN_BLOCK_H
